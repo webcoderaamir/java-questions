697ff437733cd3787958723893c0bfcb8d74059c
Aamir Ayub <aamirayub889@gmail.com>
2024-02-18 17:59:51 +0530

    adding more question in conceptOne file

--- Functions.java
+++ Functions.java
@@ -2,29 +2,114 @@ import java.util.*;
 
 public class Functions {
 
+    // Ques. Recursive functions
+    // public static int printFactorial(int n){
+    //     if(n < 0) return 0;
+
+    //     if(n == 0){
+    //         return 1;
+    //     }
+    //     // Recursive case: n! = n * (n-1)!
+    //     return n * printFactorial( n - 1 );
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Check Factorial Number : ");
+    //     int n = sc.nextInt();
+
+    //     int result = printFactorial(n);
+    //     System.out.println("Result : " + result);
+    // }
+
+
+
+    // Ques. Make a function to print the table of a given number n.
+    // public static void printTable(int n){
+    //     for(int i = 1; i < 11; i++){
+    //         System.out.println(i*n);
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Print The Table : ");
+    //     int n = sc.nextInt();
+
+    //     printTable(n);
+    // }
+
+
+
+    // ques.Make a function to check if a given number n is even or not.
+    // public static void isEven(int n){
+    //     if(n % 2 == 0){
+    //         System.out.println("isEven");
+    //     } else {
+    //         System.out.println("isOdd");
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Check Even Number : ");
+    //     int n = sc.nextInt();
+
+    //     isEven(n);
+    // }
+
+
+
+    // Ques. Make a function to check if a number is prime or not.
+    // public static void isPrime(int n){
+    //     if(n <= 1){
+    //         System.out.println("Not a prime number");
+    //         return;
+    //     }
+    //     boolean prime = true;
+    //     for(int i = 2; i <= Math.sqrt(n); i++){
+    //         if(n % i == 0){
+    //             prime = false;
+    //             break;
+    //         }
+    //     }
+
+    //     if(prime){
+    //         System.out.println("Prime number");
+    //     } else {
+    //         System.out.println("Not a prime number");
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Check Prime Number : ");
+    //     int n = sc.nextInt();
+
+    //     isPrime(n);
+    // }
+
+
+
     // ques factorial 5! = 5!*4!*3!*2!*1! 
-    public static void printFactorial(int n) {   //declare function
-        //condition -ve no.
-        if(n < 0){
-            System.out.println("Invalid Number");
-            return;
-        }
+    // public static void printFactorial(int n) {   //declare function
+    //     //condition -ve no.
+    //     if(n < 0){
+    //         System.out.println("Invalid Number");
+    //         return;
+    //     }
 
-        int factorial = 1;
-        //loop
-        for(int i = n; i >= 1; i--){
-            factorial = factorial * i;
-        }
-        System.out.println(factorial);
-        return;
-    }
-    public static void main(String[] args) {   //call function
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
+    //     int factorial = 1;
+    //     //loop
+    //     for(int i = n; i >= 1; i--){
+    //         factorial = factorial * i;
+    //     }
+    //     System.out.println(factorial);
+    //     return;
+    // }
+    // public static void main(String[] args) {   //call function
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter a number: ");
+    //     int n = sc.nextInt();
 
-        printFactorial(n);
-    }
+    //     printFactorial(n);
+    // }
 
 
     
---
+++ conceptOne.java
@@ -0,0 +1,233 @@
+import java.util.*;
+
+public class conceptOne {
+
+   // Ques 10. Write a program to print Fibonacci series of n terms where n is input by user :
+    // 0 1 1 2 3 5 8 13 21 ..... 
+    // In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+    public static void fibonacciValue(int n) {
+        int a = 0;
+        int b = 1;
+
+        System.out.print(a+" ");
+
+        if (n > 1) {
+            //find nth term
+            for (int i = 2; i <= n; i++) {
+                System.out.print(b+" ");
+                //the concept below is called swapping
+                int temp = b;
+                b = a + b;
+                a = temp;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number of terms : ");
+        int n = sc.nextInt();
+
+        fibonacciValue(n);
+    }
+
+
+
+    // Ques 9. Write a function that calculates the Greatest Common Divisor of 2 numbers.
+    // public static void findGCD(int n1, int n2) {
+    //     while (n1 != n2) {
+    //         if (n1 > n2) {
+    //             n1 = n1 - n2;
+    //         } else {
+    //             n2 = n2 - n1;
+    //         }
+    //     }
+    //     System.out.println("GCD is : " + n2);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter your first number : ");
+    //     int n1 = sc.nextInt();
+
+    //     System.out.print("Enter your second number : ");
+    //     int n2 = sc.nextInt();
+
+    //     findGCD(n1, n2);
+    // }
+    
+
+
+
+    // Ques 8. Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
+    // public static void powerValue(int x, int n) {
+    //     int result = 1;
+    //     for (int i = 1; i <= n; i++) {
+    //         result = result * x;
+    //     }
+    //     System.out.println(result);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter x Number : ");
+    //     int x = sc.nextInt();
+
+    //     System.out.print("Enter n Number : ");
+    //     int n = sc.nextInt();
+
+    //     powerValue(x, n);
+    // }
+
+
+
+    // Ques 7. Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered.
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     int postive = 0, negative = 0, zeros = 0;
+    //     System.out.print("Enter press 1 to Continue & 0 to Exit : ");
+    //     int input = sc.nextInt();
+
+    //     while (input == 1) {
+    //         System.out.print("Enter your Number : ");
+    //         int num = sc.nextInt();
+
+    //         if (num > 0) {
+    //             postive++;
+    //         } else if (num < 0) {
+    //             negative++;
+    //         } else {
+    //             zeros++;
+    //         }
+
+    //         System.out.print("Press 1 to Continue & 0 to Exit : ");
+    //         input = sc.nextInt();
+    //     }
+
+    //     System.out.println("Positive number : " + postive);
+    //     System.out.println("Negative number : " + negative);
+    //     System.out.println("Zero number : " + zeros);
+    // }
+
+
+
+    // Ques 6. Write an infinite loop using do while condition.
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     int i = 0;
+    //     do {
+    //         System.out.println(i++);
+    //     } while (true);
+    // }
+
+
+
+    // Ques 5. Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+    // public static void eligibleToVote(int age){
+    //     if(age >= 18){
+    //         System.out.println("Eligible to vote");
+    //     } else {
+    //         System.out.println("Not eligible to vote");
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter your Age : ");
+    //     int age = sc.nextInt();
+
+    //     eligibleToVote(age);
+    // }
+
+
+
+    // Ques 4. Write a function that takes in the radius as input and returns the circumference of a circle.
+    // public static void calculateCirc(int radius){
+    //     double circumference = 2 * Math.PI * radius;
+    //     System.out.println("Circumference of Circle : " + circumference);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter Radius: ");
+    //     int radius = sc.nextInt();
+
+    //     calculateCirc(radius);
+    // }
+
+
+
+    // Ques 3. Write a function which takes in 2 numbers and returns the greater of those two.
+    // public static void greaterNumber(int a, int b){
+    //     if(a > b){
+    //         System.out.println(a);
+    //     } else {
+    //         System.out.println(b);
+    //     }
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter first number: ");
+    //     int a = sc.nextInt();
+
+    //     System.out.print("Enter second number: ");
+    //     int b = sc.nextInt();
+
+    //     greaterNumber(a, b);
+    // }
+
+
+
+    // Ques 2. Write a function to print the sum of all odd numbers from 1 to n.
+    // public static void sumOfOddNumbers(int n){
+    //     int sum = 0;
+    //     for (int i = 1; i <= n; i += 2){
+    //         sum += i;
+    //     }
+    //     System.out.println(sum);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter odd Number : ");
+    //     int n = sc.nextInt();
+
+    //     sumOfOddNumbers(n);
+    // }
+
+
+    
+    // Ques 1. Enter 3 numbers from the user & make a function to print their average.
+    // public static void findAverage(int a, int b, int c){
+    //     int sum = a + b + c;
+    //     double average = (double) sum / 3;
+    //     System.out.println("Average : " + average);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter first number: ");
+    //     int a = sc.nextInt();
+
+    //     System.out.print("Enter second number: ");
+    //     int b = sc.nextInt();
+
+    //     System.out.print("Enter third number: ");
+    //     int c = sc.nextInt();
+
+    //     findAverage(a, b, c);
+    // }
+
+}
+
+    
+
+
+
+    
+
+
