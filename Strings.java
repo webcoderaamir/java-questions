7516d938e4432d67c637acc7b62f9a425d756e20
Aamir Ayub <aamirayub889@gmail.com>
2024-02-22 23:43:31 +0530

    String lecture Java

---
+++ Strings.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        // compare
+        String name1 = "Spider";
+        String name2 = "Spider";
+
+        // 1. s1 > s2 : +ve value
+        // 2. s1 == s2 : 0
+        // 3. s1 < s2 : -ve value
+
+        if(name1.compareTo(name2)) {
+        }
+
+    }
+
+    // public static void main(String[] args) {
+    //     // concatenation of strings
+    //     String FirstName = "Tony";
+    //     String SecondName = "Stark";
+    //     String FullName = FirstName + "@" + SecondName;
+    //     // Tony@Stark
+    //     System.out.println(FullName.length());
+    //     // charAt
+    //     for (int i = 0; i < FullName.length(); i++) {
+    //         System.out.println(FullName.charAt(i));
+    //     }
+    // }
+
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     String name = sc.nextLine();
+    //     System.out.println("full name : " + name);
+    // }
+
+}
