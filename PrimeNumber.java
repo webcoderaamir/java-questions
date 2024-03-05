d0de24d2c889fa569bff4f4f919b2838d92cb26d
Aamir Ayub <aamirayub889@gmail.com>
2024-03-05 09:12:16 +0530

    length, charat, substring(0, 2), parseInt, add

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,30 +1,71 @@
 import java.util.*;
-
 public class PrimeNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        boolean isPrime = true;
-
+    public static boolean isPrime(int n) {
         if (n <= 1) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
+        return true;
+    }
 
-        if (isPrime) {
-            System.out.println(n + " is a prime number.");
-        } else {
-            System.out.println(n + " is not a prime number.");
+    public static int findNthPrime(int n) {
+        int count = 0;
+        int num = 2;
+        while (count < n) {
+            if (isPrime(num)) {
+                count++;
+            }
+            if (count == n) {
+                return num;
+            }
+            num++;
         }
+        return -1;
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the index no. : ");
+        int input = scanner.nextInt();
+        scanner.close();
+        int primeNumber = findNthPrime(input);
+        if (primeNumber != -1) {
+            System.out.println(input + "th : " + primeNumber);
+        } else {
+            System.out.println("Invalid!");
+        }
     }
 }
+
+// public class PrimeNumber {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+
+//         boolean isPrime = true;
+
+//         if (n <= 1) {
+//             isPrime = false;
+//         } else {
+//             for (int i = 2; i <= Math.sqrt(n); i++) {
+//                 if (n % i == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         if (isPrime) {
+//             System.out.println(n + " is a prime number.");
+//         } else {
+//             System.out.println(n + " is not a prime number.");
+//         }
+
+//     }
+// }
--- Strings.java
+++ Strings.java
@@ -1,21 +1,130 @@
 import java.util.*;
-
+// String are immutable
 public class Strings {
 
-    public static void main(String[] args) {
-        // compare
-        String name1 = "Spider";
-        String name2 = "Spider";
+    // Ques. Input an email from the user. You have to create a username from the email by deleting the part that comes after ‘@’. Display that username to the user.
 
-        // 1. s1 > s2 : +ve value
-        // 2. s1 == s2 : 0
-        // 3. s1 < s2 : -ve value
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the email : ");
+    //     String email= sc.next();
+    //     String username = "";
 
-        if(name1.compareTo(name2)) {
-        }
+    //     for (int i = 0; i < email.length(); i++) {
+    //         if (email.charAt(i) == '@') {
+    //             break;
+    //         } else {
+    //             username += email.charAt(i);
+    //         }
+    //     }
+    //     System.out.println(username);
 
-    }
+    //     // String username = email.substring(0, email.indexOf("@"));
+    //     // System.out.println(username);
+    // }
 
+
+
+
+    // Ques. Input a string from the user. Create a new string called ‘result’ in which you will replace the letter ‘e’ in the original string with letter ‘i’.
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the string : ");
+    //     String str = sc.next();
+    //     String result = "";
+
+    //     for (int i = 0; i < str.length(); i++) {
+    //         if (str.charAt(i) == 'e') {
+    //             result += 'i';
+    //         } else {
+    //             result += str.charAt(i);
+    //         }
+    //     }
+    //     System.out.println(result);
+
+    //     // String result = str.replace('e', 'i');
+    //     // System.out.println(result);
+    // }
+    
+
+
+
+    // Ques. Take an array of Strings input from the user & find the cumulative (combined) length of all those strings.
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter size : ");
+    //     int size = sc.nextInt();
+    //     String array[] = new String[size];
+    //     int totLength = 0;
+
+    //     for (int i = 0; i < size; i++) {
+    //         array[i] = sc.next();
+    //         totLength += array[i].length();
+    //     }
+    //     System.out.println(totLength);
+    // }
+
+
+
+    // Ques. ToString Method of String class.
+    // public static void main(String[] args) {
+    //     int num = 123;
+    //     String str = Integer.toString(num);
+    //     System.out.println(str.length());
+    // }
+
+
+
+    // Ques. ParseInt Method of Integer class.
+    // public static void main(String[] args) {
+    //     String str = "123";
+    //     int num = Integer.parseInt(str);
+    //     System.out.println(num);
+    // }
+
+
+
+    // Ques. substring
+    // public static void main(String[] args) {
+    //     // compare
+    //     // String sentence = "My name is Aamir";
+    //     // String name = sentence.substring(11, sentence.length());
+
+    //     String sentence = "AamirAyub";
+    //     String name = sentence.substring(5);
+    //     System.out.println(name);
+
+    // }
+
+
+
+    // Ques. compareTo()
+    // public static void main(String[] args) {
+    //     // compare
+    //     String name1 = "Spider";
+    //     String name2 = "Spider";
+
+    //     // 1. s1 > s2 : +ve value
+    //     // 2. s1 == s2 : 0
+    //     // 3. s1 < s2 : -ve value
+
+    //     if(name1.compareTo(name2) == 0) {
+    //         System.out.println("equal");
+    //     } else {
+    //         System.out.println("not equal");
+    //     }
+
+    //     // if(new String("Tony") == new String("Tony")) {
+    //     //     System.out.println("equal");
+    //     // } else {
+    //     //     System.out.println("not equal");
+    //     // }
+
+    // }
+
+
+    // Ques. length & charAt
     // public static void main(String[] args) {
     //     // concatenation of strings
     //     String FirstName = "Tony";
@@ -23,7 +132,7 @@ public class Strings {
     //     String FullName = FirstName + "@" + SecondName;
     //     // Tony@Stark
     //     System.out.println(FullName.length());
-    //     // charAt
+    //     // charAt => break words
     //     for (int i = 0; i < FullName.length(); i++) {
     //         System.out.println(FullName.charAt(i));
     //     }
